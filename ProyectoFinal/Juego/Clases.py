393f6fa181e2d5655dbe597a77cb1ef52e0de9be
MishelCenteno <mishelcenteno96@hotmail.com>
2017-02-15 01:08:31 -0500

    Actualizaciones

--- ProyectoFinal/Juego/Clases.py
+++ ProyectoFinal/Juego/Clases.py
@@ -6,15 +6,13 @@ class Cursor(pygame.Rect):
     def update(self):
         self.left,self.top=pygame.mouse.get_pos()
 
-class Boton(pygame.sprite.Sprite):
+class Boton():
     def __init__(self,imagen1,imagen2,x,y):
-        self.imagenNormal = imagen1
+        self.imagenNormal = imagen1  
         self.imagenSeleccion = imagen2
         self.imagenActual= self.imagenNormal
         self.sonido = pygame.mixer.Sound("audio/laser.ogg")
         self.continuar = True
-        self.x=0
-        self.y=0
         self.rect = self.imagenActual.get_rect()
         self.rect.left,self.rect.top = (x,y)
 
